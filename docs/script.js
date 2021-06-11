b4684b825609a84be710ecda4e2d2c79c6bd23d7
DuarteNRP <duarte_pereira_5@hotmail.com>
2021-06-11 18:39:15 +0100

    final commit

---
+++ docs/script.js
@@ -0,0 +1,40 @@
+function rulesButton1(){
+  const rules = document.getElementById("rules1");
+  const span = document.getElementsByClassName("close")[0];
+  console.log(span)
+  rules.style.display = "block";
+  span.onclick = function() {
+    rules.style.display = "none";
+  }
+
+}
+function rulesButton2(){
+  const rules = document.getElementById("rules2");
+  const span = document.getElementsByClassName("close")[1];
+  console.log(span)
+  rules.style.display = "block";
+  span.onclick = function() {
+    rules.style.display = "none";
+  }
+
+}
+function rulesButton3(){
+  const rules = document.getElementById("rules3");
+  const span = document.getElementsByClassName("close")[2];
+  console.log(span)
+  rules.style.display = "block";
+  span.onclick = function() {
+    rules.style.display = "none";
+  }
+
+}
+function rulesButton4(){
+  const rules = document.getElementById("rules4");
+  const span = document.getElementsByClassName("close")[3];
+  console.log(span)
+  rules.style.display = "block";
+  span.onclick = function() {
+    rules.style.display = "none";
+  }
+
+}
